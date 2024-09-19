11e03065ee0651fe546c8c53d59770582bf74a26
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-09-19 21:29:59 +0000

    corrected circular import issue

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,12 +1,14 @@
+# api/v1/views/__init__.py
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-from api.v1.views.session_auth import *
-from api.v1.views.users import *
-from api.v1.views.index import *
+from . import index
+from . import users
+from . import session_auth
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import views here to avoid circular import issues
 
 User.load_from_file()
